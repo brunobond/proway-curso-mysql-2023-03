c38df30eb5ca04d350f3e91cd770333d3d54f6fb
Bruno Bond Carrenho <eng.bond@gmail.com>
2023-03-19 16:01:26 -0300

    Atualizado data de nascimento do José

--- Aula 01.sql
+++ Aula 01.sql
@@ -142,3 +142,5 @@ ALTER TABLE alunos ADD COLUMN data_nascimento DATE;
 -- Consultar a data de nascimento dos alunos
 SELECT id, nome, data_nascimento FROM alunos;
 
+-- Atualizar a data de nascimento do José
+UPDATE alunos SET data_nascimento = '2000-06-28' WHERE id=1
